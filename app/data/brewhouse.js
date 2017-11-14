02abf5aadd563a19c2e507a4e8da77e3711bf37b
Jhaydavis <Jhaydavis@mac.com>
2017-11-13 20:34:36 -0600

    added age modal

--- app/data/brewhouse.js
+++ app/data/brewhouse.js
@@ -2,28 +2,28 @@ module.exports = [{
         "name": "Lone Star Taps & Caps",
         "location": " 2823 St Louis St, Dallas, TX 75226",
         "photo": "http://www.star-telegram.com/living/food-drink/4h3hzb/picture115133063/alternates/FREE_640/taps1",
-        "answers": ["1", "1", "1", "1", "1"]
+        "answers": ["4", "2", "3", "2", "3"]
     },
 
     {
         "name": "Bishop Cider Company",
         "location": "509 N Bishop Ave Dallas, Texas 75208",
         "photo": "http://www.wfaa.com/img/resize/content.wfaa.com/photo/2016/08/25/0825%20BRAINDEAD%20BREWING_1472178960667_5838162_ver1.0.jpg?preset=video-still",
-        "answers": ["5", "2", "5", "4", "5"]
+        "answers": ["4", "2", "2", "4", "5"]
     },
 
     {
         "name": "On Rotation",
         "location": "7328 Gaston Ave  Dallas, Texas 75214",
         "photo": "https://lh3.ggpht.com/p/AF1QipPgBcYwpeCdp4ugUtAJP_4IwLetkdXwfV_8aCc=w512-h384-n",
-        "answers": ["1", "2", "5", "4", "5"]
+        "answers": ["3", "3", "5", "4", "3"]
     },
 
     {
         "name": "Texas Ale Project",
         "location": "2032 Executive Dr.  Dallas, Texas 75041",
         "photo": "https://media.licdn.com/media/AAEAAQAAAAAAAAOEAAAAJDBkZmRjZjEzLTg2ODMtNDAzYy1hMGU5LWM4OWM1MTI4MmYxZA.png",
-        "answers": ["3", "5", "5", "4", "5"]
+        "answers": ["4", "2", "4", "4", "4"]
     },
 
 
@@ -32,14 +32,14 @@ module.exports = [{
         "location": "2823 St Louis St, Dallas, TX 75226",
         "map": "https://www.google.com/",
         "photo": "https://static.urbandaddy.com/uploads/assets/image/articles/standard/47f8bdcb4ab269922cb8ae0394f338ddeddb8f58.jpg",
-        "answers": ["5", "5", "5", "5", "5"]
+        "answers": ["3", "3", "5", "3", "5"]
     },
 
     {
         "name": "Four Corners Brewing Company",
         "location": "1311 S Ervay St.  Dallas, Texas 75215",
         "photo": "https://media-cdn.tripadvisor.com/media/photo-s/06/b9/ce/5b/four-corners-brewing.jpg",
-        "answers": ["1", "3", "1", "2", "3"]
+        "answers": ["3", "3", "2", "2", "3"]
     },
 
     {
@@ -47,7 +47,7 @@ module.exports = [{
         "name": "Oak Highlands Brewery",
         "location": "10484 Brockwood Rd  Dallas, Texas 75238",
         "photo": "https://images1.dallasobserver.com/imager/u/745xauto/8500265/ohb_3.jpg",
-        "answers": ["1", "2", "5", "4", "5"]
+        "answers": ["5", "1", "2", "4", "3"]
     },
 
 
@@ -55,21 +55,21 @@ module.exports = [{
         "name": "Nobel Ray Brewing Co",
         "location": "2636 Farrington St. Dallas, Texas 75207",
         "photo": "https://www.wanderu.com/blog/wp-content/uploads/2016/10/noble-rey-interior-680.jpg",
-        "answers": ["3", "2", "5", "4", "5"]
+        "answers": ["4", "2", "5", "4", "3"]
     },
 
     {
         "name": "Peticolas Brewing",
         "location": "2026 Farrington St Dallas, Texas 75207",
         "photo": "http://www.velvethammer5k.com/images/w5.jpg",
-        "answers": ["1", "2", "5", "4", "5"]
+        "answers": ["2", "4", "3", "4", "2"]
     },
 
     {
         "name": "Braindead Brewing",
         "location": "2625 Main St  Dallas, Texas 75226",
         "photo": "https://lh3.ggpht.com/p/AF1QipPnto0Wy1zNRjI0SEjLMlp_A7LcFoSWlqAFgcYZ=w682-h384-n",
-        "answers": ["1", "2", "5", "4", "5"]
+        "answers": ["3", "3", "4", "4", "3"]
     },
 
 
@@ -77,7 +77,7 @@ module.exports = [{
         "name": "Community Beer Company",
         "location": "1530 Inspiration Dr.  Dallas, Texas 75207",
         "photo": "https://images1.dallasobserver.com/imager/u/original/7028122/dsc_0039.jpg",
-        "answers": ["5", "2", "5", "4", "5"]
+        "answers": ["1", "5", "3", "4", "3"]
     }
 
 ];
