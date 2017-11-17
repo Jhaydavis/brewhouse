e83eb483fb0622ae0cce12706700d4fd59f03ea1
Jhaydavis <Jhaydavis@mac.com>
2017-11-17 15:31:30 -0600

    heroku db updates

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,20 +1,31 @@
-// *********************************************************************************
-// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
-// *********************************************************************************
-
-// Dependencies
+// Set up database connection
+var mysql = require('mysql');
 var Sequelize = require("sequelize");
+var connection;
 
-// Creates mySQL connection using Sequelize
-var sequelize = new Sequelize("hoppy_reviews", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
+//  JAWSDB - heroku
+if (process.env.JAWSDB_URL) {
+	connection = Sequelize.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = new Sequelize("hoppy_reviews", "root", "", {
+		host: "localhost",
+		dialect: "mysql",
+		pool: {
+		  max: 5,
+		  min: 0,
+		  idle: 10000
+		}
+	  });
+};
+
+
+connection.connect(function(err) {
+	if (err) {
+		console.error('error conencting: ' + err.stack);
+		return;
+	}
+	console.log('connected as id ' + connection.threadId);
 });
 
-// Exports the connection for other files to use
-module.exports = sequelize;
+// export the connection back to orm
+module.exports = connection;
